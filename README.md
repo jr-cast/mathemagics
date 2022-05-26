eb9d9f57697d033b11b6ac81eb51e4881b43f742
jr-cast <castanos@pm.me>
2022-05-26 16:13:49 -0500

    Check linters and update readme file

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ![](https://img.shields.io/badge/Microverse-blueviolet)
 
-# Mathemagics: Events 🎟️
+# Mathemagics: Refactor with hooks 🪝
 
 > "Mathemagics" is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to:
 
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,17 @@
-const Button = ({ className, text, onClick }) => {
-  return <button className={className} type="button" text={text} onClick={onClick}>{text}</button>
-}
+import PropTypes from 'prop-types';
+
+const Button = ({ className, text, onClick }) => <button className={className} type="button" text={text} onClick={onClick}>{text}</button>;
+
+Button.defaultProps = {
+  className: 'grid-item',
+  text: 'template',
+  onClick: 'compute',
+};
+
+Button.propTypes = {
+  className: PropTypes.string,
+  text: PropTypes.string,
+  onClick: PropTypes.func,
+};
 
 export default Button;
